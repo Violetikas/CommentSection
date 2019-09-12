a330ee9d2687d00f03487d87731ad79346a71f8a
violetatamasauskiene <violetikas@yahoo.com>
2019-09-12 19:51:03 +0300

    Security fixes

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ $comments = $getComment->getAll();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css 'sha256-iLRIDfWbrDWQ0NijtGVqwoRhn5cqp8kcgZ3cKPZGXUw=';">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CommentSection</title>
--- views/comment.php
+++ views/comment.php
@@ -1,4 +1,4 @@
-<div class="container card mb-5" style="background-color: #dddddd"
+<div class="container card mb-5"
      data-comment-id="<?php echo $comment['comment_id'] ?>">
     <div class="card-header">
         <strong><?php echo $comment['name']; ?></strong>
@@ -6,17 +6,17 @@
         <button type="button" class="btn btn-light btn-sm button-reply float-right">Reply</button>
     </div>
     <div class="card-body">
-        <?php echo $comment['comment']; ?>
+        <?php echo htmlentities($comment['comment']); ?>
     </div>
     <?php foreach ($comments[$comment['comment_id']] ?? [] as $childComment): ?>
-        <div class="container card mb-5" style="size: 70em; background-color: #dddddd"
-             data-comment-id="<?php echo $childComment['comment_id'] ?>">
+        <div class="card-reply container card mb-5"
+             data-comment-id="<?php echo htmlentities($childComment['comment_id'])?>">
             <div class="card-header">
-                <strong><?php echo $childComment['name']; ?></strong>
-                <?php echo $childComment['date']; ?>
+                <strong><?php echo htmlentities($childComment['name']); ?></strong>
+                <?php echo htmlentities($childComment['date']); ?>
             </div>
             <div class="card-body">
-                <?php echo $childComment['comment']; ?>
+                <?php echo htmlentities($childComment['comment']); ?>
             </div>
         </div>
     <?php endforeach; ?>
